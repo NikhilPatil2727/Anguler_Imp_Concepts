44b1eec542f8ba144d117112bab7e5adf3fe8a65
NikhilPatil2727 <nikhilpatil02436@gmail.com>
2026-05-27 15:45:31 +0530

    Implement counter functionality and add login component
    - Updated app.component.html to include counter display and buttons for incrementing, decrementing, and resetting the counter.
    - Modified app.component.ts to manage counter state and handle button actions.
    - Created login.component.ts, login.component.html, and login.component.css for the new login component.
    - Added a test file for the LoginComponent to ensure it is created successfully.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+  import { Component } from '@angular/core';
+  import { RouterOutlet } from '@angular/router';
+  import { LoginComponent } from './login/login.component';
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-export class AppComponent {
-  title = 'practice';
-}
+  @Component({
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet,LoginComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
+  })
+  export class AppComponent {
+    //properties
+    title = 'blast';
+    name = 'Angular';
+
+    // handleClick(){
+    //   this.otherFunctin();
+    //   alert("Button Clicked");
+    // }
+
+    // otherFunctin(){
+    //   console.log("Other Function Called");
+    // }
+      
+    count= 0;
+    
+    increment(){
+      this.count++;
+    }
+
+    decrement(){
+      this.count--;
+    }
+
+    reset(){
+      this.count=0;
+    }
+  }
